11e0fafaa3c919c9578c12c968d3ba9d31df88ae
“Poojanmodi” <modipoojan444@gmail.com>
2021-12-01 23:08:20 +0530

    UC1: First Name Validated

---
+++ src/com/javaRegex/User_Main.java
@@ -0,0 +1,16 @@
+package com.javaRegex;
+
+import java.util.Scanner;
+
+public class User_Main {
+
+	public static void main(String[] args) {
+		System.out.println("\nUser Registration\n");
+		Scanner scan = new Scanner(System.in);
+		User_Registration call = new User_Registration();
+		System.out.println("Enter First Name: \n");
+		String fname = scan.next();
+		System.out.println(call.validate_firstName(fname));
+	scan.close();	
+	}
+}
