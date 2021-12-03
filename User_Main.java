49ac18a1f754e15c986796baf671fa814e385ac9
modi-poojan <80687912+modi-poojan@users.noreply.github.com>
2021-12-03 05:56:36 +0530

    UC9: All emails conditions validated

--- User_Main.java
+++ User_Main.java
@@ -10,25 +10,25 @@ public class User_Main {
 		
 		User_Registration call = new User_Registration();
 		
-		System.out.println("Enter First Name: \n");
+		/*System.out.println("Enter First Name: \n");
 		String fname = scan.next();
 		System.out.println(call.validate_firstName(fname));
 		
 		System.out.println("Enter Last Name: \n");
 		String lname = scan.next();
-		System.out.println(call.validate_lastName(lname));		
+		System.out.println(call.validate_lastName(lname));	*/	
 
 		System.out.println("Enter email: \n");
 		String email = scan.next();
 		System.out.println(call.validate_E_Mail(email));
 		
-		System.out.println("Enter Mobile Number: \n");
+		/*System.out.println("Enter Mobile Number: \n");
 		String number = scan.next();
 		System.out.println(call.validate_E_Mail(number));
 		
 		System.out.println("Enter Password: \n");
 		String password = scan.next();
-		System.out.println(call.validate_E_Mail(password));
+		System.out.println(call.validate_E_Mail(password));*/
 		
 		
 	scan.close();	
